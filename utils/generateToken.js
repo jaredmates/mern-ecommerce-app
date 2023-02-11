587560bad8981dbca026d4e862dff505ce5a6bfb
Jared Mates <jaredmates45@gmail.com>
2023-02-10 18:16:01 -0800

    added generateToken

---
+++ utils/generateToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+// Generate JWT
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+module.exports = generateToken;
